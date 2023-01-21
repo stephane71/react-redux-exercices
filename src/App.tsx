a3b3679846a8fd5713b8f348fd2c39e6e35770cd
stephane71 <stephane@un-bourguignon.com>
2023-01-21 20:34:40 +0100

    App | add a state to handle selectedCity

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Cities from "./assets/cities.json";
 import "./App.css";
 import CityOverviewCard from "./components/CityOverviewCard";
 
 function App() {
+  const [selectedCity, setSelectedCity] = useState(null);
+
   function handleClickCity(city) {
     return () => {
-      alert(city.nom);
+      setSelectedCity(city);
     };
   }
 
   return (
     <div className="App">
       <h1>Formation React et Redux</h1>
+      {selectedCity && <div>Votre sélection: {selectedCity.nom}</div>}
       <div>
         {Cities.map((city) => (
           <CityOverviewCard
