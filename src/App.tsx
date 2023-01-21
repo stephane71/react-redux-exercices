e8e99cdf5e982e07c7788d7450d8a50caa4cf9dd
stephane71 <stephane@un-bourguignon.com>
2023-01-21 19:53:31 +0100

    CityOverview | handle data from "city" prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Cities from "./assets/cities.json";
 import "./App.css";
 import CityOverview from "./components/CityOverview";
 
 function App() {
-  return <div className="App">
-    <h1>Formation React et Redux</h1>
-    <div>
-      <CityOverview />
+  return (
+    <div className="App">
+      <h1>Formation React et Redux</h1>
+      <div>
+        <CityOverview city={Cities[20]} />
+        <CityOverview city={Cities[565]} />
+        <CityOverview city={Cities[450]} />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
--- src/components/CityOverview/index.tsx
+++ src/components/CityOverview/index.tsx
@@ -1,12 +1,11 @@
 import styles from "./CityOverview.module.css";
-import Cities from "../../assets/cities.json";
 
 const IMG_SIZE = 50;
 const EMPTY_FLAG = "./logo192.png";
 
-const CITY = Cities[20];
+export default function CityOverview({ city }) {
+  const { nom: name, code } = city;
 
-export default function CityOverview() {
   function handleError(e) {
     const { target } = e;
     target.src = EMPTY_FLAG;
@@ -16,14 +15,14 @@ export default function CityOverview() {
     <div className={styles.CityOverview}>
       <img
         alt="flag"
-        src={`./${CITY.nom}${CITY.code}.svg`}
+        src={`./${name}${code}.svg`}
         width={IMG_SIZE}
         height={IMG_SIZE}
         onError={handleError}
       />
       <div className={styles.right}>
-        <div>{CITY.nom}</div>
-        <div>{CITY.code}</div>
+        <div>{name}</div>
+        <div>{code}</div>
       </div>
     </div>
   );
