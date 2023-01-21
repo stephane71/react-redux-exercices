a42e5ca1f1cb0ccc35e8886e6a907e99a2ff88c3
stephane71 <stephane@un-bourguignon.com>
2023-01-21 20:19:31 +0100

    CityOverview | add message when population > NB_RESIDENTS_LIMIT

--- src/components/CityOverview/index.tsx
+++ src/components/CityOverview/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./CityOverview.module.css";
+import { NB_RESIDENTS_LIMIT } from "../../utils/enums";
 
 const IMG_SIZE = 50;
 const EMPTY_FLAG = "./logo192.png";
 
 export default function CityOverview({ city }) {
-  const { nom: name, code } = city;
+  const { nom: name, code, population } = city;
 
   function handleError(e) {
     const { target } = e;
@@ -23,6 +24,9 @@ export default function CityOverview({ city }) {
       <div className={styles.right}>
         <div>{name}</div>
         <div>{code}</div>
+        {population > NB_RESIDENTS_LIMIT * 3 ? (
+          <div className={styles.residentsInfo}>{`+ de 300 000 hbs`}</div>
+        ) : null}
       </div>
     </div>
   );
